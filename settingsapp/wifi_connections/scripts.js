dddf483e9e880be27f708430d7aef2629d4a4220
Kacper <kacperkawec481@gmail.com>
2026-02-09 17:27:15 +0000

    rrrr

--- settingsapp/wifi_connections/scripts.js
+++ settingsapp/wifi_connections/scripts.js
@@ -6,7 +6,7 @@ document.getElementById("showwifionoroff").innerHTML = toggleshow;
 }
 }
 
-/*I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:DI am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:DI am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D*/
+/*I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:DI am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D I am a comment. I do nothing. If you're using DevTools, I just exist to annoy you >:D*/
 function onoffwifi(){
 let toggle = toggle-1;
 if (toggle===-1){
@@ -17,7 +17,10 @@ if (toggle===-1){
 function toggleshowsettings(){
     while (foreversssss===1){
     while (toggle===1){
-let toggleshow = "On"
+let toggleshow = "On";
     }
-    }
+while (toggle===0){
+    let toggleshow = "Off";
+}
+}
 }
