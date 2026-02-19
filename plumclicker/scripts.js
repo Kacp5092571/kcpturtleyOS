59d93fa98f4d1b430de8b2bc029f5d4434875ae6
Kacper <kacperkawec481@gmail.com>
2026-02-19 21:09:01 +0000

    add more to plum clicker

--- plumclicker/scripts.js
+++ plumclicker/scripts.js
@@ -1,9 +1,15 @@
-let clickss = 0
+let clickss = 0    
+let timeusedorwasted = 0
 function addtheclicks(){
-    clickss = clickss + 1;
-    document.getElementById('pointsdisplay').innerHTML = clickss;
+  clickss = clickss + 1;
+setInterval(function otherthing(){
+    document.getElementById('pointsdisplay').innerHTML = clickss;    
     if (clickss > 29){
         alert('wow you actually spent your time doing this');
-        alert('screenshot this to show off to your friends for absolutley no reason')
+        alert('screenshot this to show off to your friends for absolutley no reason');
     };
+}, 500)
+setInterval(function otherotherthing(){
+timeusedorwasted = timeusedorwasted + 1;
+}, 1000)  
 }
