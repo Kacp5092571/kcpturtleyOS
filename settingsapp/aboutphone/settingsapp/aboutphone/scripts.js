71b27e9a566bffafccfd8068f6e9e053eeb59098
Kacper <kacperkawec481@gmail.com>
2026-02-19 12:41:11 +0000

    href

--- settingsapp/aboutphone/settingsapp/aboutphone/scripts.js
+++ settingsapp/aboutphone/settingsapp/aboutphone/scripts.js
@@ -3,6 +3,6 @@ function AddClicks(){
     clicks = clicks + 1;
     console.log(clicks);
     if (clicks > 6){
-        window.location.href='https://kacp5092571.github.io/kcpturtleyOS/settingsapp/aboutphone/?-?-?-?-?-?'
+        window.location.href='https://kacp5092571.github.io/kcpturtleyOS/settingsapp/aboutphone/secretsecretsecretsecretsecretsecretsecret'
     }
 }
