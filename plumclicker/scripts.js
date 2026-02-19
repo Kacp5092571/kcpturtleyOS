b2683b2079c110c81b9fef6c387b7ca760142b03
Kacper <kacperkawec481@gmail.com>
2026-02-19 12:30:07 +0000

    aaa

--- plumclicker/scripts.js
+++ plumclicker/scripts.js
@@ -2,7 +2,7 @@ let clickss = 0
 function addtheclicks(){
     clickss = clickss + 1;
     document.getElementById('pointsdisplay').innerHTML = clickss;
-    if (clicks > 99){
+    if (clickss > 29){
         alert('wow you actually spent your time doing this');
         alert('screenshot this to show off to your friends for absolutley no reason')
     };
