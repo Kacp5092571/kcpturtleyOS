1e1e25ec1b187b5c56b14da2a691ef9abb889f35
Kacper <kacperkawec481@gmail.com>
2026-02-22 15:03:22 +0000

    add checkmate thing to legacyanim and add some polish

---
+++ pllang.js
@@ -0,0 +1,10 @@
+tak = 0
+function właczpljęzyk(){
+    let tak = 1
+document.getElementById("text_settings").innerHTML = "Ustawienia"
+document.getElementById("text_app_time").innerHTML = "aplikacja do czasu? (i ta aplikacja nie jest do godziny!)"
+document.getElementById("plum_clicker").innerHTML = "Kliker śłiwki"
+    if (tak = 0){
+        window.location.href = 'https://www.google.com'
+    }
+}
