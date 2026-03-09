ee938362a251f3b35e1b196182b3a174681817ac
Kacper <kacperkawec481@gmail.com>
2026-03-09 17:19:29 +0000

    aaaaaaaaaza

--- plumclicker/scripts.js
+++ plumclicker/scripts.js
@@ -1,8 +1,12 @@
 let clickss = 0    
 let timeusedorwasted = 0
+let plumwords = " plum/s"
+let fullthing = " "
 function addtheclicks(){  
-    clickss = clickss + 1;    
-    document.getElementById('pointsdisplay').innerHTML = clickss;   
+    clickss = clickss + 1;
+clicksstr = toString(clicks);
+fullthing = clicksstr + plumwords;
+    document.getElementById('pointsdisplay').innerHTML = fullthing;   
   if (clickss > 29){
         alert('wow you actually spent your time doing this');
         alert('screenshot this to show off to your friends for absolutley no reason');
