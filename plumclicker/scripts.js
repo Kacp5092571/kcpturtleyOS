a6077adc6143b40e5fdba586294340fb0328ce29
Kacper <kacperkawec481@gmail.com>
2026-03-16 16:43:11 +0000

    add more stuff add force autoplay using htmkl video

--- plumclicker/scripts.js
+++ plumclicker/scripts.js
@@ -1,7 +1,8 @@
 let clickss = 0    
 let timeusedorwasted = 0
+let pm = 1
 function addtheclicks(){  
-    clickss = clickss + 1;
+    clickss = clickss + 1*pm;
     document.getElementById('pointsdisplay').innerHTML = clickss;   
   if (clickss > 29){
         alert('wow you actually spent your time doing this');
@@ -12,4 +13,16 @@ function addtheclicks(){
 setInterval(function otherotherthing(){
 timeusedorwasted = timeusedorwasted + 1;
 }, 1000)  
+}
+
+function dcpc(){
+if(clicks>49){
+pm = 2;
+}else{
+  let clickslefttodc = 50-clickss;
+  let clicksl2dcstr = str(clickslefttodc);
+   let space = " "
+  let fullthingg = "SCAMMER. YOU HAVE" + clicksl2dcstr + space + "left to unlock double clicks."
+  alert(fullthingg)
+}
 }
