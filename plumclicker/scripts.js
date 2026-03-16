7d7fd0b12ccf789a19e52ac6150d3a1284970e1d
Kacper <kacperkawec481@gmail.com>
2026-03-16 17:11:11 +0000

    a

--- plumclicker/scripts.js
+++ plumclicker/scripts.js
@@ -5,7 +5,7 @@ function addtheclicks(){
   console.log("If you're seeing this, then WHY ARE YOU IN THE JAVASCRIPT CONSOLE??")  
     clickss = clickss + 1*pm;
     document.getElementById('pointsdisplay').innerHTML = clickss;   
-  if (clickss = 1000){
+  if (clickss > 1000){
         alert('wow you actually spent your time doing this');
         alert('screenshot this to show off to your friends for absolutley no reason');
         console.log("idk");
