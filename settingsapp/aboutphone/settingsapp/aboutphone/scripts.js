5ea78f26ad25ee3241824e8aef11c1c07683f42a
Kacper <kacperkawec481@gmail.com>
2026-03-23 21:01:22 +0000

    Update scripts.js

--- settingsapp/aboutphone/settingsapp/aboutphone/scripts.js
+++ settingsapp/aboutphone/settingsapp/aboutphone/scripts.js
@@ -4,6 +4,6 @@ function AddClicks(){
     clicks = clicks + 1;
     console.log(clicks);
     if (clicks > 6){
-        window.location.href='https://kacp5092571.github.io/kcpturtleyOS/settingsapp/aboutphone/secretsecretsecretsecretsecretsecretsecret'
+        window.location.href='https://kacp5092571.github.io/kcpturtleyOS/settingsapp/aboutphone/secretgameayy'
     }
 }
