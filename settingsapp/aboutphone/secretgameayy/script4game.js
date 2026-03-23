eaa57f2de82a5b3e7b5a16a54174f5b44fc1ad9e
Kacper <kacperkawec481@gmail.com>
2026-03-23 17:53:06 +0000

    aaaaa

--- settingsapp/aboutphone/secretgameayy/script4game.js
+++ settingsapp/aboutphone/secretgameayy/script4game.js
@@ -1,16 +1,17 @@
 let moving = 0;
 let count = 0;
 function move(){
-    let thingmoved = 100;
+    let thingmoved = 20;
     moving = 1
     while (moving===1){
         document.getElementById("carr").style.right = `${thingmoved}px`;
         count = count+1;
-if (count === 40){
+
+}
+if (count===40){
 alert('CRASSHHHHH');
-}
-}
-    }   
+}    
+}   
 
 function stop(){
     moving = 0;
