0c4ab1b706f84de3fc3080aa972a9b237097734c
Kacper <kacperkawec481@gmail.com>
2026-04-06 16:22:49 +0100

    add new easter egg game

--- settingsapp/aboutphone/secretgameayy/script4game.js
+++ settingsapp/aboutphone/secretgameayy/script4game.js
@@ -1,19 +1,16 @@
-let moving = 0;
-let count = 0;
-function move(){
-    let thingmoved = 20;
-    moving = 1
-    while (moving===1){
-        setInterval(function frmove() {
-    document.getElementById("carr").style.right = `${thingmoved}px`;
-        count = count+1;
-        }, 1000)
-if (count===40){
-alert('CRASSHHHHH');
-} 
-}
-}   
-
-function stop(){
-    moving = 0;
+let moveright = 22
+let moveleft = -22
+let count = 0
+let plugged = 0
+alert("Low battery. Please plug in charger.")
+alert("Battery life - N/A seconds remaining")
+function tryplug(){
+    if (count === 3) {
+     plugged = 1
+    } else {
+    count = count + 1
+    }
+    if (plugged = 1) {
+        alert('insert charging noises')
+    }
 }
