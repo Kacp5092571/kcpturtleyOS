fee3a3175e4e81bd2806c2a29bc117957e86305e
Kacper <kacperkawec481@gmail.com>
2026-04-06 20:22:18 +0100

    script moreee 4 game - 0.6

--- settingsapp/aboutphone/secretgameayy/script4game.js
+++ settingsapp/aboutphone/secretgameayy/script4game.js
@@ -1,20 +1,21 @@
-let count = 0
+let count = 1
 let plug = 0
 let side = 1
 function tryplug(){
     if (count < 3) {
         count = count + 1
-        if (count === 1) {
+        if(count === 1){
             side = 1
-        document.getElementById("dacable").src = "side1.png"
-        }
-        if (count === 2){
-            side = 2
-            document.getElementById("dacable").src = "side2.png"
-        }
-    } else {
-      side = 0
+document.getElementById("dacable").src = "side1.png"
+    } 
+    if (count === 2){
+        side = 2
+        document.getElementById("dacable").src = "side2.png"
+    }
+    }else {
       plug = 1
+      document.getElementById("dacable").src = "side1.png"
       alert('yay')  
     }
+
 }
