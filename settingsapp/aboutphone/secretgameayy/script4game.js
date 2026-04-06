3ded51fb47271bce217f382134d7153d422b35f3
Kacper <kacperkawec481@gmail.com>
2026-04-06 19:45:37 +0100

    add new pics

--- settingsapp/aboutphone/secretgameayy/script4game.js
+++ settingsapp/aboutphone/secretgameayy/script4game.js
@@ -1,10 +1,19 @@
 let count = 0
 let plug = 0
-
+let side = 1
 function tryplug(){
     if (count < 3) {
         count = count + 1
+        if (count === 1) {
+            side = 1
+        document.getElementById("dacable").src = "side1.png"
+        }
+        if (count === 2){
+            side = 2
+            document.getElementById("dacable").src = "side2.png"
+        }
     } else {
+      side = 0
       plug = 1
       alert('yay')  
     }
