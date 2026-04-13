7e44da9ccb27263491a50a08a916dde3beea8961
Kacper <kacperkawec481@gmail.com>
2026-04-13 17:16:38 +0100

    re-add the aboutphobne page cuz it got deleted/lost for sm reason - 0.6.5

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,2 +1,3 @@
 # Contributing to Turtley OS
 You can contribute or help, just ask!
+Please only do things that are appropriate (for obvious reasons)
--- games/musicthing/scripts.js
+++ games/musicthing/scripts.js
@@ -1,4 +1,4 @@
-let drumactive = 0;
+let drumactivity = 0;
 var drummy = document.getElementById=("glassbreaking.mp3");
 function drumactive(){
 drummy.play();
