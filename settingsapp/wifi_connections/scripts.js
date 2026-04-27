765fc1f7823f94a267b071a3165e606d0b6d5120
Kacper <kacperkawec481@gmail.com>
2026-04-27 16:43:07 +0100

    more scripts for wifiandconnection toggle

--- settingsapp/wifi_connections/scripts.js
+++ settingsapp/wifi_connections/scripts.js
@@ -1,13 +1,13 @@
-let toggle = 1;
-let toggleinfo = "On"
-function toggleit(){
-    if (toggle = 1){
-        toggleinfo = "On";
-    toggle = 0;
-    }
-    if (toggle = 0){
-        toggleinfo = "Off";
-        toggle = 1;
-    }
-    document.getElementById("toggleinfo").innerHTML = toggleinfo;
+let toggledis = "On";
+let toggvalue = 1;
+function toggleswitch(){
+ if (toggvalue === 1){
+ toggvalue = 0;
+ toggledis = "Off";
+ }
+ if (toggvalue === 0){
+    toggvalue = 1;
+    toggledis = "On";
+ }
+ document.getElementById("toggshowyh").innerHTML = toggledis;
 }
