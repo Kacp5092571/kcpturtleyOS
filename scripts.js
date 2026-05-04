c6794dce537b116f5c011b697c6ee69d17d9b6a2
Kacper <kacperkawec481@gmail.com>
2026-05-04 12:43:35 +0100

    changeback

--- scripts.js
+++
@@ -1,6 +0,0 @@
-function startupy(){
-const audio = document.getElementById('turtley os startup by mike');
-audio.play();
-alert('this worked!');
-window.location.href='https://kacp5092571.github.io/kcpturtleyOS/'; 
-}
---
+++ startup/scripts.js
@@ -0,0 +1,5 @@
+const audio = document.getElementById('turtley os startup by mike');
+audio.play();
+alert('this worked!');
+window.location.href='https://kacp5092571.github.io/kcpturtleyOS/'; 
+
