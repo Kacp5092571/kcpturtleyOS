a40dd613c524ecba069f7bc63676bef138bc7556
Kacper <kacperkawec481@gmail.com>
2026-05-09 11:34:03 +0100

    add more to devtools

--- settingsapp/aboutphone/credits/scripts.js
+++ settingsapp/aboutphone/credits/scripts.js
@@ -1,15 +1,20 @@
 function devtools(){
     let devtools = 1
 if (devtools === 1){
-    let choice = prompt("a - crazy mode");
+    let choice = prompt("a - crazy mode b - Load legacy game files");
     if (choice === "a"){
         let thing = prompt('purple (a) or weird (b)?')
         if (thing === "a"){
         document.getElementById("hi").style.backgroundColor = "purple";
-      }    
-      if (thing = "b"){
+       if (thing = "b"){
         document.getElementById("hi").style.backgroundColor = "honeydew"
       }
+      }
+     }
+     if (choice === "b"){
+      alert("Loading system files...")
+      window.location.href='https://kacp5092571.github.io/kcpturtleyOS/secretgamelegacy/'
+      alert('Complete!')
      }
   }
 }
