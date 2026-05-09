2f338a2427a7140f3c4f015012c988f3542de5a0
Kacper <kacperkawec481@gmail.com>
2026-05-09 11:48:27 +0100

    make new variable so alert wont repeat

--- secretgamelegacy/scripts.js
+++ secretgamelegacy/scripts.js
@@ -1,11 +1,17 @@
 let changes = 0
+let notagain = 0
 let textmessage = "BANANA"
 function changething(){
+    if (notagain === 0){
     let keypro = prompt("Access key?")
 if (keypro !== "02OG"){
   alert("Wrong. Restarting software...")
   window.location.href='https://kacp5092571.github.io/kcpturtleyOS/startup/'
-}
+}else{
+  notagain = 1
+}    
+    }
+
 changes = changes + 1;
      if (changes ===1){
       textmessage = "BANANa";
