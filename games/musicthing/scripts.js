1357ccf1a68abb61d1ae2b0756ba265282fb6ac5
Kacper <kacperkawec481@gmail.com>
2026-05-11 17:22:28 +0100

    add storage webpage (unfinished)

--- games/musicthing/scripts.js
+++ games/musicthing/scripts.js
@@ -2,5 +2,5 @@ let drumactivity = 0;
 var x = document.getElementById=("glassbreaking.mp3");
 function drumactive(){
 x.play();
-setTimeout(() => x.pause, 5000);
+setTimeout(() => x.pause(), 5000);
 }
