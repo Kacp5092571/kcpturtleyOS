2e024d4751ab8ab716d54a57f72f50293933a3c2
Kacper <kacperkawec481@gmail.com>
2026-06-01 17:49:52 +0100

    more scripts WAY MORE FRFR - ult0.3

--- ultimate/scripts.js
+++ ultimate/scripts.js
@@ -1,3 +1,28 @@
 function biggamestext(){
     document.getElementById("gamespara").style.fontSize = "50px";
 }
+
+let secl = 0
+document.addEventListener('keydown', event)
+
+if(event.key=== 'ArrowDown'){
+    secl = secl + 1
+    if (secl===1){
+        document.getElementById("hi").style.fontSize = "30px"
+    }else{
+        document.getElementById("hi").style.fontSize = "16px"
+        if (secl===2){
+            document.getElementById("hi2").style.fontSize = "30px"
+        }else{
+document.getElementById("hi2").style.fontSize = "16px";
+if (secl===3){
+    document.getElementById("hi3").style.fontSize = "30px";
+}else{
+    document.getElementById("hi3").style.fontSize = "16px";
+}
+if (secl===4){
+    secl=1
+}
+        }
+    }
+}
