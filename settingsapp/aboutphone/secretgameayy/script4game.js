0935fba6281d582651afb6b69e24e09af8fa6a29
Kacper <kacperkawec481@gmail.com>
2026-06-08 17:43:28 +0100

    debugging

--- settingsapp/aboutphone/secretgameayy/script4game.js
+++ settingsapp/aboutphone/secretgameayy/script4game.js
@@ -1,5 +1,5 @@
 let moving = 10
-const countries = [poland, warsaw, gdansk, mazury, bialystok]
+const countries = ["poland", "warsaw", "gdansk", "mazury", "bialystok"]
 
 function move(){
     document.getElementById('sailboat').style.transform = "translate(50px, 0px)"
