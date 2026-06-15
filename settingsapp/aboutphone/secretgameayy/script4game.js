6e3899731acbfde1c3831fb8da24316d16320c1c
Kacper <kacperkawec481@gmail.com>
2026-06-15 16:54:18 +0100

    HUGE update to the game

--- settingsapp/aboutphone/secretgameayy/script4game.js
+++ settingsapp/aboutphone/secretgameayy/script4game.js
@@ -1,6 +1,31 @@
 let moving = 10
+let distance = 0
+let paddle = 2
+let speedboost = 0
 const countries = ["poland", "warsaw", "gdansk", "mazury", "bialystok"]
 
-function move(){
-    document.getElementById('sailboat').style.transform = "translate(50px, 0px)"
+function paddle(){
+distance = distance + paddle
+document.getElementById("paddle").innerHTML = distance
+if (distance===200){
+    alert('SPEED BOOSTER found! Press OK to continue.')
+    speedboost = 1
+    document.getElementById("spbstxt").innerHTML = "Speed booster"
+}
+if (distance === 1000){
+document.getElementById("mainbody").style.backgroundColor = "skyblue"
+}
+if (distance===2000){
+    document.getElementById("mainbody").style.backgroundColor = "darkblue"
+}
+}
+
+function usespeedboost(){
+    if(speedboost === 1){
+         distance = distance + paddle * 70
+         alert('zooooooooom')
+         document.getElementById("spbstxt").innerHTML = "-"
+    }else{
+        alert('nope')
+    }
 }
