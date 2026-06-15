01973bf8fc905182b129c02bb49f2bedce42ca23
Kacper <kacperkawec481@gmail.com>
2026-06-15 17:30:11 +0100

    mokre

--- settingsapp/aboutphone/secretgameayy/script4game.js
+++ settingsapp/aboutphone/secretgameayy/script4game.js
@@ -1,28 +1,28 @@
 let moving = 10
-let distancee = 0
+let distanceee = 0
 let paddle = 2
 let speedboost = 0
 const countries = ["poland", "warsaw", "gdansk", "mazury", "bialystok"]
 
 function paddles(){
-distance = distance + paddle
-document.getElementById("disshow").innerHTML = distancee
-if (distance===200){
+distanceee = distanceee + paddle
+document.getElementById("disshow").innerHTML = distanceee
+if (distancee===200){
     alert('SPEED BOOSTER found! Press OK to continue.')
     speedboost = 1
     document.getElementById("spbstxt").innerHTML = "Speed booster"
 }
-if (distance === 1000){
+if (distancee === 1000){
 document.getElementById("mainbody").style.backgroundColor = "skyblue"
 }
-if (distance===2000){
+if (distancee===2000){
     document.getElementById("mainbody").style.backgroundColor = "darkblue"
 }
 }
 
 function usespeedboost(){
     if(speedboost === 1){
-         distance = distance + paddle * 70
+         distancee = distancee + paddle * 70
          alert('zooooooooom')
          document.getElementById("spbstxt").innerHTML = "none"
     }else{
