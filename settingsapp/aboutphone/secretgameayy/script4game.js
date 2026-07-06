6f5b315a1c69093f1ebcb93961b8985f17303564
Kacper <kacperkawec481@gmail.com>
2026-07-06 17:19:28 +0100

    more to sailing sim@Kacp5092571

--- settingsapp/aboutphone/secretgameayy/script4game.js
+++ settingsapp/aboutphone/secretgameayy/script4game.js
@@ -1,6 +1,11 @@
 let moving = 10
 let distancee = 0
 let paddle = 1
+let side = "right"
+let sidecount = 0
+let deggs = 60
+let deggsback = 210
+let degdis = 0
 let speedboost = 0
 const countries = ["poland", "warsaw", "gdansk", "mazury", "bialystok"]
 let random = Math.floor(Math.random() * 180)
@@ -8,7 +13,11 @@ let random2 = Math.floor(Math.random() * 360)
 
 function paddles(){
 distancee = distancee + paddle
+sidecount = sidecount + 1
+degdis = degdis + 60
 document.getElementById("disshow").innerHTML = distancee
+document.getElementById("degshow").innerHTML = degdis
+document.getElementById("salilboat").style.transform =`rotate${deggs}deg`
 if (distancee===random){
     alert('SPEED BOOSTER found! Press OK to continue.')
     speedboost = 1
@@ -25,7 +34,7 @@ document.getElementById("mainbody").style.backgroundColor = "skyblue"
 if (distancee===2000){
     document.getElementById("mainbody").style.backgroundColor = "darkblue"
 }
-const myTimeout = setTimeout(paddlepart2(), 600)
+const myTimeout = setTimeout(paddlepart2(), 1000)
 }
 
 function paddlepart2(){
@@ -50,5 +59,19 @@ distancee = distancee + 500
 
     if (codes==="paddleop"){
         paddle = 20
+    }
+    document.getElementById("disshow").innerHTML = distancee;
+}
+
+function switching(){
+    sidecount = 0;
+    degdis = 0;
+    document.getElementById("salilboat").style.transform =`rotate${deggsback}deg`
+    if (side==="right"){
+side = "left"        
+    }
+
+    if (side==="left"){
+    side="right"
     }
 }
