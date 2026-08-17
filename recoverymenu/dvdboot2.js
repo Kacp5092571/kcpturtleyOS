6d052b3aab88115eb00c3012506930da23a662aa
Kacper <kacperkawec481@gmail.com>
2026-08-17 09:38:31 +0100

    recovery menu and sequence for bsod

---
+++ recoverymenu/dvdboot2.js
@@ -0,0 +1,9 @@
+let thing = prompt("New Device Wizard, continue? (Y/N)")
+if (thing==="Y"){
+    let thing2 = prompt("Would you like to insert "TURTLEYOS" DVD drive? (Y/N)")
+    if(thing2==="Y"){
+        alert("BOOTING TURTLEYOSRECOV...")
+        window.location.href='https://kacp5092571.github.io/kcpturtleyOS'
+        alert("Operation successful!")
+    }
+}
