ec8d098894d38891f5cd5c6b347acb4b3d37bee0
Kacper <kacperkawec481@gmail.com>
2026-09-14 17:24:04 +0100

    hi

--- stupidgame/scripts.js
+++ stupidgame/scripts.js
@@ -1,7 +1,7 @@
 function catwowWOW(){
-    document.getElementById("catbruh").style.display = "block"
+    document.getElementById("catbruh").style.display = "block@
 }
 
 function rickrolling(){
-    
+
 }
