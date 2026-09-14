97215178ee07e9d07f447f4a3aa3ed4ac659dcef
Kacper <kacperkawec481@gmail.com>
2026-09-14 17:37:14 +0100

    more

--- stupidgame/scripts.js
+++ stupidgame/scripts.js
@@ -1,7 +1,24 @@
 function catwowWOW(){
-    document.getElementById("catbruh").style.display = "block@
+    document.getElementById("catbruh").style.display = "block"
 }
 
 function rickrolling(){
+alert("NEVER GONNA GIVE YOU UP")
+alert("NEVER GONNA LET YOU DOWN")
+alert("hello")
+}
 
+function win(){
+    alert("YOU WIN")
+    alert("what are the odds...")
+    alert("it could've been luck...")
+    alert("But who cares, you're bannmed for 1 second!")
+    window.location.href='https://kacp5092571.github.io/kcpturtleyOS'
+    alert("Welcome! Hope you wasted your time 60 seconds ago!")
+}
+
+function crashyay(){
+    alert("click if you hate yourself")
+    document.write("LOSER")
+    crashyay()
 }
