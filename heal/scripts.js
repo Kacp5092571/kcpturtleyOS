b09d8d8003c89aeae098b18aa26101ce5390dcdb
Kacper <kacperkawec481@gmail.com>
2026-09-21 17:50:01 +0100

    aaa

--- heal/scripts.js
+++ heal/scripts.js
@@ -1,15 +1,15 @@
 let hp=10
 let hpshow="-"
-healfunc(){
+function ealfunc(){
     hp=hp+10
 hpshow = hpshow+" - "
 }
 
+function thingss(){
 setInterval(function thing() {
         hp = hp + 1;
         hpshow = hpshow - " - "
 document.getElementById("hpdis").innerHTML = hpshow;    
 }, 1000)
-   } 
-    }
+}
 
